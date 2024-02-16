ea98e8cb2ac3a2023bb25e0384a3d52932c71873
charuzain <charuabhizain@gmail.com>
2024-02-16 01:56:00 -0500

    Add profile detail and rating

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -15,31 +15,7 @@ function Avatar() {
           <FontAwesomeIcon icon={faCircleCheck} />
         </div>
       </div>
-      <div className="information">
-        <div className="details">
-          <p className="name">John Doe</p>
-          <p className="date">Joined: January 1, 2022</p>
-          <p className="location">Location: New York</p>
-          <div className="rating">
-            <p>Rating:</p>
-            {/* Stars component */}
-            <div className="stars">{/* Your stars component goes here */}</div>
-            <p>4.5 (100)</p>
-          </div>
-        </div>
 
-        {/* Icons */}
-        <div className="icons">
-          <div className="icon">Phone</div>
-          <div className="icon">Email</div>
-          <div className="icon">Facebook</div>
-        </div>
-      </div>
-
-      {/* Active status */}
-      <div className="active-status">
-        <p>Active 10 minutes ago</p>
-      </div>
     </div>
   );
 }
--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -1,9 +1,33 @@
-import Avatar from "../../components/Avatar/Avatar"
+import Avatar from '../../components/Avatar/Avatar';
+import './UserProfile.scss';
 
 const UserProfile = () => {
-  return (
-    <Avatar/>
-  )
-}
+  const renderStars = (rating) => {
+    const stars = [];
+    for (let i = 0; i < rating; i++) {
+      stars.push(
+        <span key={i} className="profile__star">
+          &#9733;
+        </span>
+      );
+    }
+    return stars;
+  };
 
-export default UserProfile
+  return (
+    <main className="profile">
+      <div className="profile__left-container">
+        <Avatar />
+        <div className="profile__details">
+          <p>James Lu</p>
+          <p>Joined Jan 2014</p>
+          <p>NorthYork, ON</p>
+          <div className="profile__rating">{renderStars(5)} (300)</div>{' '}
+        </div>
+      </div>
+      <p>KijiJi Verified Member</p>
+    </main>
+  );
+};
+
+export default UserProfile;
