39d0c11533faba0e259cefdddecf01dedc09aeca
charuzain <charuabhizain@gmail.com>
2024-02-16 13:55:42 -0500

    Fix error

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
-import './App.css';
-import UserProfile from './pages/UserProfile/UserProfile';
-import Social from "./pages/Social/Social"
-
 import './App.scss';
 import UserProfile from './pages/UserProfile/UserProfile';
-import EscrowServicePage from "./pages/EscrowServicePage";
-import ProductPage from "./pages/ProductPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Social from './pages/Social/Social';
+
+import EscrowServicePage from './pages/EscrowServicePage';
+import ProductPage from './pages/ProductPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
-    <>
-      <UserProfile/>
-      <Social/>
-    </>
-  );
-}
-
-export default App;
-      <BrowserRouter>
-        <main>
-          <Routes>
-            <Route path="/product" element={<ProductPage />} />
-            <Route path="/escrowService" element={<EscrowServicePage />} />
-           <Route path="/" element={<UserProfile/>} />
-          </Routes>
-        </main>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/product" element={<ProductPage />} />
+        <Route path="/social" element={<Social />} />
+        <Route path="/escrowService" element={<EscrowServicePage />} />
+        <Route path="/" element={<UserProfile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
