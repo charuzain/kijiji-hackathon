14a437b80620e87d925faaecdac8089192c067fb
Bita Fotovvat <bita.ftvt@gmail.com>
2024-02-15 22:17:26 -0500

    initial commit & some changes

---
+++ src/components/Social/Social.js
@@ -0,0 +1,18 @@
+import "./Social.scss";
+import Logo from "../../styles/assets/logo.png";
+import SearchBar from "../../styles/assets/searchbar.png";
+
+export default function Social(){
+    
+
+    return(
+    <>
+    <header className="header">
+        <img src="Logo" alt="logo image" />
+        <img src="SearchBar" alt="search bar" />
+
+    </header>
+
+    </>
+    );
+}
