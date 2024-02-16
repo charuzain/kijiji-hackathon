d9f64e4e2933b0d04be3013e8aefbbfdd691a682
Chao Meng <cmeng036@gmail.com>
2024-02-15 22:22:47 -0500

    Revert "Merge pull request #1 from charuzain/feature/escrow"
    This reverts commit 09573c394f7e8d67b7aeb706596b1ea7e7b0083c, reversing
    changes made to 514af782903aafcd163e344d51ba08d650d34c6e.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
+import './App.scss';
 
-import "./App.css";
-import EscrowServicePage from "./pages/EscrowServicePage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-function App() {
+export default function App() {
   return (
-    <>
-      <BrowserRouter>
-        <main>
-          <Routes>
-            <Route path="/escrowService" element={<EscrowServicePage />} />
-          </Routes>
-        </main>
-      </BrowserRouter>
-      <h1>KIJIJI</h1>
-    </>
-
+    <></>
   );
 }
 
